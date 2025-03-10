3fd0f34b29acdb6ae8f1a526349cd5bfb83c9d1c
alejandro <josued.ad13@gmail.com>
2025-03-09 21:32:57 -0600

    limitando el origen

--- config/cors.js
+++ config/cors.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ['*'] // Dominios permitidos
+const allowedOrigins = ['190.242.24.66'] // Dominios permitidos
 
 const CORS_OPTIONS = {
   origin: function (origin, callback) {
